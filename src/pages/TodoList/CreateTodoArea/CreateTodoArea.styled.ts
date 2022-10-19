5c6553c9ecf54151edcd429d67ee632c2953a837
jindonyy <rsefa94@gmail.com>
2022-10-19 23:16:40 +0900

    Feat: CreateTodoArea 구현

---
+++ src/pages/TodoList/CreateTodoArea/CreateTodoArea.styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import TextInput from '@/components/common/TextInput';
+
+const $Form = styled.form`
+  ${({ theme }) => theme.mixins.flexBox({ justify: 'center' })};
+  gap: 1rem;
+  width: 40rem;
+  margin-top: 4rem;
+`;
+
+const $AddInput = styled(TextInput)`
+  flex: 1;
+`;
+
+export { $Form, $AddInput };
---
+++ src/pages/TodoList/CreateTodoArea/index.tsx
@@ -0,0 +1,48 @@
+import Button from '@/components/common/Button';
+import { debounce } from '@/utils/eventDelay';
+import { INPUT_DELAY } from '@/constants/time';
+import useInputValue from '@/hooks/useInputValue';
+import { $Form, $AddInput } from '@/pages/TodoList/CreateTodoArea/CreateTodoArea.styled';
+import { FormEvent } from 'react';
+import { fetchTodoToCreate } from '@/api/todo';
+import { hasFetchError } from '@/utils/error';
+
+type CreateTodoAreaProp = {
+  getTodos: () => Promise<void>;
+};
+
+export default function CreateTodoArea({ getTodos }: CreateTodoAreaProp) {
+  const { inputValue, updateInputValue } = useInputValue({
+    create: ''
+  });
+
+  const createTodo = async () => {
+    const payload = {
+      todo: inputValue.create
+    };
+    const response = await fetchTodoToCreate(payload);
+
+    if (!hasFetchError(response.statusCode)) getTodos();
+  };
+
+  const handleCreateButton = (event: FormEvent) => {
+    event.preventDefault();
+
+    createTodo();
+  };
+
+  return (
+    <$Form name="todo" onSubmit={handleCreateButton}>
+      <$AddInput
+        name="create"
+        placeholder="할 일을 추가해주세요."
+        required
+        defaultValue={inputValue.create}
+        onChange={debounce(updateInputValue, INPUT_DELAY)}
+      />
+      <Button type="submit" size="small">
+        추가 +
+      </Button>
+    </$Form>
+  );
+}
